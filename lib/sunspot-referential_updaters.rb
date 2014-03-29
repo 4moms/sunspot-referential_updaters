4da4af5379853e8422ff7015c9990fdc65a314ce
Dan Carper <dcarper@dreamagile.com>
2014-03-29 14:14:30 -0400

    main functionality done

--- lib/sunspot-referential_updaters.rb
+++ lib/sunspot-referential_updaters.rb
@@ -1,15 +1,24 @@
 require "sunspot-referential_updaters/version"
 
 module Sunspot
-  module ReferencialUpdaters
-		def self.included
+  module ReferentialUpdaters
+		def self.included(base)
 			base.extend(ClassMethods)
+		end
+
+		def reindex_relations!
+			self.class.reindexable_relations.each do |relation|
+				relation.solr_index
+			end
 		end
 
 		module ClassMethods
 			def reindexes(*relations)
-
+				self.reindexable_relations = relations
+				after_save :reindex_relations!
 			end
+
+			attr_accessor :reindexable_relations
 		end
   end
 end
--- spec/sunspot/referential_updaters_spec.rb
+++ spec/sunspot/referential_updaters_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 
+class Library
+	include Sunspot::ReferentialUpdaters
+end
+
 describe Sunspot::ReferentialUpdaters do
 	describe '##reindexes' do
 		it 'should accept and store N relations to reindex' do
-			class Library < Object; end
-
-			Library.include Sunspot::ReferentialUpdaters
+			Library.stubs(:after_save)
 			Library.reindexes :books, :authors
-			Library.class_variable_get(:relations_to_reindex).should == [:books, :authors]
+			Library.reindexable_relations.should == [:books, :authors]
+		end
+
+		it 'should send #after_save to the base with the :reindex_relations symbol' do
+			Library.expects(:after_save).with(:reindex_relations!)
+			Library.reindexes
+		end
+	end
+
+	describe '#reindex_relations!' do
+		it 'should call #solr_index on all relations' do
+			library = Library.new
+			relation1 = mock(:solr_index)
+			relation2 = mock(:solr_index)
+			Library.stubs(:reindexable_relations).returns([relation1, relation2])
+			library.send :reindex_relations!
 		end
 	end
 end
