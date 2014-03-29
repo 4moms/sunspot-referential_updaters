cd158c575b12eb556ba0507e5f0d0ec060570ef8
Dan Carper <dcarper@dreamagile.com>
2014-03-29 14:56:21 -0400

    railtie doesnt work if namespaced

--- lib/sunspot/referential_updaters/railtie.rb
+++ lib/sunspot/referential_updaters/railtie.rb
@@ -1,9 +1,5 @@
-module Sunspot
-	module ReferentialUpdaters
-		class Railtie < Rails::Railtie
-			initializer 'railtie.configure_rails_initialization' do
-				::ActiveRecord::Base.send :include, Sunspot::ReferentialUpdaters
-			end
-		end
-	end
+class MyRailtie < Rails::Railtie
+  initializer "my_railtie.configure_rails_initialization" do
+		ActiveRecord::Base.send :include, Sunspot::ReferentialUpdaters
+  end
 end
