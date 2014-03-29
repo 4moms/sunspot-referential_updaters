6f8311fa5db812e127e828578e02ed390ae772c0
Dan Carper <dcarper@dreamagile.com>
2014-03-29 14:39:19 -0400

    reoganizes things better

--- lib/sunspot-referential_updaters.rb
+++ lib/sunspot-referential_updaters.rb
@@ -1,24 +1,6 @@
-require "sunspot-referential_updaters/version"
+require "sunspot/referential_updaters/version"
+require "sunspot/referential_updaters"
 
-module Sunspot
-  module ReferentialUpdaters
-		def self.included(base)
-			base.extend(ClassMethods)
-		end
-
-		def reindex_relations!
-			self.class.reindexable_relations.each do |relation|
-				relation.solr_index
-			end
-		end
-
-		module ClassMethods
-			def reindexes(*relations)
-				self.reindexable_relations = relations
-				after_save :reindex_relations!
-			end
-
-			attr_accessor :reindexable_relations
-		end
-  end
+if defined?(Rails)
+	require 'sunspot/referential_updaters/railtie'
 end
---
+++ lib/sunspot/referential_updaters.rb
@@ -0,0 +1,22 @@
+module Sunspot
+  module ReferentialUpdaters
+		def self.included(base)
+			base.extend(ClassMethods)
+		end
+
+		def reindex_relations!
+			self.class.reindexable_relations.each do |relation|
+				relation.solr_index
+			end
+		end
+
+		module ClassMethods
+			def reindexes(*relations)
+				self.reindexable_relations = relations
+				after_save :reindex_relations!
+			end
+
+			attr_accessor :reindexable_relations
+		end
+  end
+end
